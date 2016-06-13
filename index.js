1590b50d5c13e103b6f345f810432c13ef5decbb
Brenton Simpson <appsforartists@google.com>
2016-06-13 17:24:21 +0200

    [removed] no-floating-decimal
    @sindresorhus has this set in xo, but I don't see any rules about it in
    the Google Style Guide.  Moreover, I see thousands of instances of
    decimals without leading zeroes when I search internally:
    
    lang:javascript \ \.\d -file:.*/third_party

--- index.js
+++ index.js
@@ -24,10 +24,6 @@ module.exports = {
     'indent': [2, 2, {
       SwitchCase: 1
     }],
-    'curly': 0,
-    'no-unused-vars': [2, {
-      "args": "none",
-    }],
     'space-before-function-paren': [2, 'never'],
     'valid-jsdoc': [2, {
       requireReturn: false,
@@ -39,6 +35,14 @@ module.exports = {
     'max-len': [1, 80, 4, {
       ignoreComments: true,
       ignoreUrls: true
-    }]
+    }],
+
+    //  Resetting things that eslint-config-xo has an opinion about, but the
+    //  Google Style Guide doesn't.
+    'curly': 0,
+    'no-floating-decimal': 0,
+    'no-unused-vars': [2, {
+      "args": "none",
+    }],
   }
 };
