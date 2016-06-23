3f5d9aa6017454d2ea76d4bb125a1434e6d9bc27
Brenton Simpson <appsforartists@google.com>
2016-06-23 15:59:00 -0700

    [added] quotes: single
    [JS Style](https://google.github.io/styleguide/javascriptguide.xml)
    says:
    
    > Prefer ' over "
    
    Honestly, it feels a bit nit-picky to me that we care about which quotes
    somebody uses, but this config should faithfully reflect what's in the
    Style Guide.  If someone has a problem with it, that person can always
    disable it downstream.

--- index.js
+++ index.js
@@ -27,6 +27,14 @@ module.exports = {
   ],
 
   rules: {
+    'quotes': [
+      'error',
+      'single',
+      {
+        avoidEscape: true,
+        allowTemplateLiterals: true
+      }
+    ],
     'indent': [
       'error',
       2,
